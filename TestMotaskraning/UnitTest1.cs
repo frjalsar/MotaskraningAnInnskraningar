f70ff1510a15734482f695c3da0ab3ac1cb1b9a4
steini96 <steini.tki@gmail.com>
2021-03-31 15:12:41 +0000

    Add the test project

---
+++ TestMotaskraning/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MotaskraningWeb;
+
+namespace TestMotaskraning
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0,0);
+        }
+    }
+}
