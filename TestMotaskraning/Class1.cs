13dc251167b52310bc534a37a3005beb9f0eaee1
steini96 <steini.tki@gmail.com>
2021-04-17 14:53:29 +0000

    Setup and attempted use

---
+++ TestMotaskraning/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMotaskraning
+{
+    class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void startBrowser()
+        {
+            driver = new ChromeDriver("D:\\Forrit\\chromedriver_win32");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://localhost:5001";
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
